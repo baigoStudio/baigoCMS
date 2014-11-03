be4a20698711b25a546b0df6e57808e3cf9f2052
fone <fone@huangyifengdeMac-Pro.local>
2014-11-03 13:43:45 +0800

    v1.0

--- bg_core/inc/version.inc.php
+++ bg_core/inc/version.inc.php
@@ -6,6 +6,6 @@
 define("PRD_CMS_POWERED", "Powered by");
 define("PRD_CMS_NAME", "baigo CMS");
 define("PRD_CMS_URL", "http://www.baigo.net/Products/baigoCMS/");
-define("PRD_CMS_VER", "1.0 beta2");
-define("PRD_CMS_PUB", "20140911");
+define("PRD_CMS_VER", "1.0");
+define("PRD_CMS_PUB", "20141103");
 ?>
